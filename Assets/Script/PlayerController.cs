95f2b149281c110e497b7410fe3a605aee3b14a3
LAPTOPZER <vakadarbhafill@gmail.com>
2025-04-27 18:53:11 +0700

    Add Collider + Effector + PlayerController Script

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb2d;
+    //Vector2 moveInput;
+    private SpriteRenderer spriteRenderer;
+
+    float move;
+    [SerializeField] float speed;
+
+    [SerializeField] float jumpForce;
+    [SerializeField] bool isJump;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        /*moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), 0 *//*Input.GetAxisRaw("Vertical")*//*);
+        rb2d.AddForce(moveInput * speed);*/
+
+        /*        move = Input.GetAxis("Horizontal");
+                rb2d.linearVelocity = new Vector2(move * speed, rb2d.linearVelocity.y);*/
+
+        move = Input.GetAxisRaw("Horizontal");
+        FlipSprite();
+        rb2d.velocity = new Vector2(move * speed, rb2d.velocity.y);
+
+        if (Input.GetButtonDown("Jump") && !isJump)
+        {
+            rb2d.AddForce(new Vector2(rb2d.linearVelocity.x, jumpForce));
+            Debug.Log("Jump");
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isJump = false;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isJump = true;
+        }
+    }
+
+    void FlipSprite()
+    {
+        if (move > 0)
+            spriteRenderer.flipX = false;
+        else if (move < 0)
+            spriteRenderer.flipX = true;
+    }
+}
